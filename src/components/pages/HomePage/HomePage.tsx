12e0219f068720de9aff275ad985311b468a3e24
Krishna Gupta <belivethatkg@gmail.com>
2023-05-20 00:12:04 +0530

    cleanup.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -16,26 +16,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-{
-  /* <div className="grid min-h-[80vh] w-full grid-cols-[1fr_1.5fr_1fr] gap-[1px]">
-        <div className="relative h-full bg-black">
-          <Image
-            src={"/code-editor/code-editor-ss-1.png"}
-            width={"800"}
-            height={"800"}
-            alt="SS of Code Editor by Krishna"
-            className="absolute left-0 top-0 w-full"
-          />
-          <Image
-            src={"/code-editor/code-editor-ss-2.png"}
-            width={"800"}
-            height={"800"}
-            alt="SS of Code Editor by Krishna"
-            className="absolute left-0 top-0 w-full"
-          />
-        </div>
-        <div className="h-full bg-black"></div>
-        <div className="h-full bg-black"></div>
-      </div> */
-}
